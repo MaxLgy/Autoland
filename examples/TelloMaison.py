9907c7e27d9d65f6c8d6f35fd78e62e125c3769a
ngnepist <stephane.ngnepiepaye@ensta-bretagne.org>
2021-03-12 15:39:47 +0100

    "estimate pose"

--- examples/TelloMaison.py
+++ examples/TelloMaison.py
@@ -3,7 +3,7 @@ import os
 #print(os.getcwd())
 import tello
 import time
-import roblib
+# import roblib
 from numpy import cos,sin,pi
 
 
@@ -11,27 +11,41 @@ class Telloperso():
     def __init__(self):
         self.tello = tello.Tello()
         self.tello.connect()
-        self.x = 0
-        self.y = 0
-        self.z = 0
+        print(f"Battery Life Pecentage: {self.tello.get_battery()}")
+        self.x = 0  # cm
+        self.y = 0  # cm
+        self.z = 0  # cm
         self.theta = 0
-        self.v = 50 #cm.s-1
-        self.vdown = 25 #cm.s-1
-        self.vrota = 60 #°.s-1
-        self.offslp = 0 #s
+
+        self.v_left_right = 0  # cm.s-1
+        self.v_forward_backw = 0  # cm.s-1
+        self.v_up_dow = 0  # cm.s-1
+        self.last_v_left_right = 0  # cm.s-1
+        self.last_v_forward_backw = 0  # cm.s-1
+        self.last_v_up_dow = 0  # cm.s-1
+        self.last_time_command = 0
+
+        self.v = 50  # cm.s-1
+        self.vdown = 25  # cm.s-1
+        self.vrota = 60  # °.s-1
+        self.offslp = 0  # s
 
     def tkoff(self):
         try:
             self.tello.takeoff()
+            self.z = 100
+            self.last_time_command = time.time()
         except:
             self.tello.land()
             print("Emergency stop")
         #time.sleep(self.offslp)
-        self.z += 60
+
+        # self.z += 60
 
     def lnd(self):
         self.tello.land()
         ##time.sleep(self.offslp + angle/self.v)
+        self.last_time_command = time.time()
 
     def fwrd(self,cm):
         "move for n cm between 20-500"
@@ -66,8 +80,16 @@ class Telloperso():
     
     def testrc(self):
         #self.tello.send_rc_control(20, 0, -20, 30)
-        self.tello.send_rc_control(0,0,0,0)
-        time.sleep(5)
+        #send_rc_control(self,left_right_velocity: int, forward_backward_velocity: int, up_down_velocity: int, yaw_velocity: int):
+        self.x += self.last_v_forward_backw * (time.time()-self.last_time_command)
+        self.y += self.last_v_left_right * (time.time()-self.last_time_command)
+        self.z += self.last_v_up_dow * (time.time()-self.last_time_command)
+        self.tello.send_rc_control(self.v_left_right, self.v_forward_backw, self.v_up_dow, 0)
+        self.last_time_command = time.time()
+        self.last_v_up_dow = self.v_up_dow
+        self.last_v_left_right = self.v_left_right
+        self.last_v_forward_backw = self.v_forward_backw
+        # time.sleep(5)
 
     def state_vector(self):
         return array([[self.x], [self.y], [self.z]])
@@ -77,3 +99,21 @@ class Telloperso():
         θ = self.tello.get_state_field('roll')
         ψ = self.tello.get_state_field('yaw')
         return eulermat(φ,θ,ψ)
+
+
+if __name__ == '__main__':
+    drone = Telloperso()
+    drone.tkoff()
+    drone.v_forward_backw = 20
+    drone.v_left_right = 0
+    drone.v_up_dow = 0
+    drone.testrc()
+    time.sleep(3)
+    drone.v_forward_backw = 0
+    drone.v_left_right = 0
+    drone.v_up_dow = 0
+    drone.testrc()
+    print("x = ", drone.x)
+    print("y = ", drone.y)
+    print("z = ", drone.z)
+    drone.lnd()
--- examples/control.py
+++ examples/control.py
@@ -14,39 +14,56 @@ print(f"Battery Life Pecentage: {tello.get_battery()}")
 
 print("Takeoff - hoser!")
 tello.takeoff()
-print("x= ", tello.get_state_field('x'))
-print("y= ", tello.get_state_field('y'))
-print("z= ", tello.get_state_field('z'))
+time.sleep(3)
+x0, y0, z0 = 0,0,100
+t0 = time.time()
+tello.send_rc_control(0, 20, 0, 0) # send_rc_control(self, left_right_velocity: int, forward_backward_velocity: int, up_down_velocity: int, yaw_velocity: int):
+time.sleep(3)
+t = time.time()
+tello.send_rc_control(0, 0, 0, 0)
+x, y, z = 20 * (t-t0) + x0, 0 * (t-t0) + y0, -0 * (t-t0) + z0
+print("x= ", x)  # tello.get_state_field('x'))
+print("y= ", y)  # tello.get_state_field('y'))
+print("z= ", z)  # tello.get_state_field('z'))
+print("pitch= ", tello.get_state_field('pitch'))
+print("roll= ", tello.get_state_field('roll'))
+print("yaw= ", tello.get_state_field('yaw'))
 print("-------------------")
 time.sleep(3)
 
-v0 = 100 # 100cm/seconde
+v0 = 100  # 100cm/seconde
 
-x, y =0,0
-xhat, yhat = 100,100
+x, y = 0, 0
+xhat, yhat = 100, 100
 
 dt = 0.01
 
 angle = m.atan2(1,1)*180/m.pi
 print("angle = ", angle)
 dist = int(100*m.sqrt(2))
-# tello.rotate_clockwise(360) #60degre/second
+# tello.rotate_clockwise(360) # 60 degre /second
 # time.sleep(2)
 # tello.move_forward(dist)
-tello.move_up(50)
-print("x= ", tello.get_state_field('x'))
-print("y= ", tello.get_state_field('y'))
-print("z= ", tello.get_state_field('z'))
+# tello.move_up(50)
+print("x= ", x)# tello.get_state_field('x'))
+print("y= ", y)#tello.get_state_field('y'))
+print("z= ", z)#tello.get_state_field('z'))
+print("pitch= ", tello.get_state_field('pitch'))
+print("roll= ", tello.get_state_field('roll'))
+print("yaw= ", tello.get_state_field('yaw'))
 print("-------------------")
-time.sleep(1)
-tello.move_forward(40)
 # time.sleep(1)
-print("x= ", tello.get_state_field('x'))
-print("y= ", tello.get_state_field('y'))
-print("z= ", tello.get_state_field('z'))
+# tello.move_forward(40)
+# time.sleep(1)
+print("x= ", x)# tello.get_state_field('x'))
+print("y= ", y)#tello.get_state_field('y'))
+print("z= ", z)#tello.get_state_field('z'))
+print("pitch= ", tello.get_state_field('pitch'))
+print("roll= ", tello.get_state_field('roll'))
+print("yaw= ", tello.get_state_field('yaw'))
 print("-------------------")
 
-time.sleep(1)
+# time.sleep(1)
 
 print("landing")
 tello.land()

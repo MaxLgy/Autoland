d561d5a619612358a1bb62cc6f5055c44120c05d
MaxLgy <maxime.legeay@ensta-bretagne.org>
2021-04-15 09:52:13 +0200

    Last version Threads

--- examples/Threads.py
+++ examples/Threads.py
@@ -6,9 +6,21 @@ import numpy as np
 from TelloMaison import Telloperso
 from guidage import *
 import copy
+import sys
+import signal
 
 mutex = threading.Lock()
 DATA_CAM = {}
+THREAD_ON = True
+
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    THREAD_ON = False
+    drone.tello.emergency()
+    
+    sys.exit(0)
+
 
 class SensorsThread(threading.Thread):
     def __init__(self, drone):
@@ -17,7 +29,7 @@ class SensorsThread(threading.Thread):
     def run(self):
         time.sleep(5)
         t = time.time()
-        while True:
+        while THREAD_ON:
             mutex.acquire()
             try:
                 data_tmp = copy.deepcopy(DATA_CAM)
@@ -32,7 +44,7 @@ class SensorsThread(threading.Thread):
                 drone.y += drone.last_v_left_right * (time.time()-drone.last_time_command)
                 drone.z += drone.last_v_up_dow * (time.time()-drone.last_time_command)
                 drone.z = drone.tello.get_distance_tof()
-            time.sleep(0.1)
+            time.sleep(0.11)
 
 
 class CommandThread(threading.Thread):
@@ -42,14 +54,15 @@ class CommandThread(threading.Thread):
     
     def run(self):
         self.drone.tkoff()
-        phat = np.array([[0], [0], [0]])
+        phat = np.array([[-150], [-100], [-10]])
         while drone.tello.get_distance_tof() > 30:
             p = np.array([[drone.x], [drone.y], [drone.z]])  # position drone
             Q = champ(p, n, phat)
-            drone.v_forward_backw = int(Q[0, 0])
-            drone.v_left_right = int(Q[0, 1])
-            drone.v_up_dow = int(Q[0, 2] * 3)
+            drone.v_forward_backw = int(Q[0, 0]*0.5)
+            drone.v_left_right = int(Q[0, 1]*0.5)
+            drone.v_up_dow = int(Q[0, 2] * 8)
             drone.testrc()
+            print(Q)
             time.sleep(0.1)
         time.sleep(0.5)
         self.drone.lnd()
@@ -60,22 +73,36 @@ class CameraThread(threading.Thread):
         threading.Thread.__init__(self)
         
     def run(self):
-        while True:
+        while THREAD_ON:
             data = cam_data_reading.read()
             mutex.acquire()
             try:
                 DATA_CAM = copy.deepcopy(data)
             finally:
                 mutex.release()
+            try:
+                drone.x = data["x"]
+                drone.y = data["y"]
+                drone.z = data["z"]
+            except:
+                drone.x += drone.last_v_forward_backw * (time.time()-drone.last_time_command)
+                drone.y += drone.last_v_left_right * (time.time()-drone.last_time_command)
+                drone.z += drone.last_v_up_dow * (time.time()-drone.last_time_command)
+                drone.z = drone.tello.get_distance_tof()
             time.sleep(0.1)
 
 
 if __name__ == "__main__":
     drone = Telloperso()
     c = CommandThread(drone)
-    s = SensorsThread(drone)
+    #s = SensorsThread(drone)
     cam = CameraThread()
+    
+    signal.signal(signal.SIGINT, signal_handler)
+
+    #signal.pause()
+    
     c.start()
-    s.start()
+    #s.start()
     cam.start()
     
--- examples/cam_data_reading.py
+++ examples/cam_data_reading.py
@@ -10,16 +10,19 @@ s.connect((HOST, PORT))
 
 def read():
     r = s.recv(9999999)
-    c = str(r).split('*')[:-1]
-    c = c[0].split(',')
+    try:
+        c = str(r).split('*')[:-1]
+        c = c[0].split(',')
+    except:
+        return {}
     data = {"id":int(c[1])}
-    data["x"] = float(c[2])
-    data["y"] = float(c[3])
-    data["z"] = float(c[4])
+    data["y"] = -float(c[2])*100
+    data["x"] = -float(c[3])*100
+    data["z"] = float(c[4])*100
     data["yaw"] = float(c[5])
     data["pitch"] = float(c[6])
     data["roll"] = float(c[7])
-    
+        
     return data
     
 

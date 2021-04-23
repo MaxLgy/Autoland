23907260d198ef1ae95681bde1004fbca049cd8e
MaxLgy <maxime.legeay@ensta-bretagne.org>
2021-04-23 15:00:04 +0200

    Some bug resolution

--- examples/Threads.py
+++ examples/Threads.py
@@ -11,12 +11,10 @@ import signal
 
 mutex = threading.Lock()
 DATA_CAM = {}
-THREAD_ON = True
 
 
 def signal_handler(sig, frame):
     print('You pressed Ctrl+C!')
-    THREAD_ON = False
     drone.tello.send_rc_control(0,0,0,0)
     drone.tello.emergency()
     
@@ -30,7 +28,7 @@ class SensorsThread(threading.Thread):
     def run(self):
         time.sleep(5)
         t = time.time()
-        while THREAD_ON:
+        while True:
             mutex.acquire()
             try:
                 data_tmp = copy.deepcopy(DATA_CAM)
@@ -54,33 +52,44 @@ class CommandThread(threading.Thread):
         self.drone = drone
     
     def run(self):
-        #self.drone.tkoff()
-        phat = np.array([[-80], [0], [-15]])
-        
-        p = np.array([[drone.x], [drone.y], [drone.z]])
-        Q = champ(p, n, phat)
-        Q = (100/np.pi)*np.arctan(Q/100)
+        self.drone.tkoff()
         #while np.linalg.norm(Q) > 3:
-        while True :#drone.tello.get_distance_tof() > 30:
-            p = np.array([[drone.x], [drone.y], [drone.z]])  # position drone
+        while drone.tello.get_distance_tof() > 30:
+            if drone.platform_coords:
+                phat = np.array([[drone.platform_coords["x"] - 40*np.cos((np.pi/180)*drone.platform_coords["yaw"])], [drone.platform_coords["y"]], [drone.platform_coords["z"] + 20]])
+            else:
+                phat = np.array([[0], [0], [0]])
+            p = np.array([[drone.x], [drone.y], [drone.z]])
             Q = champ(p, n, phat)
             Q = (100/np.pi)*np.arctan(Q/100)
-            print("Drone yaw", drone.yaw)
-            drone.v_yaw = 0#-int(((drone.yaw - 90)))
-            print("vyaw",drone.v_yaw)
             
+            if drone.reversed:
+                Q[0, 0] = - Q[0, 0]
+                Q[0, 1] = - Q[0, 1]
             
-            drone.v_forward_backw = 0 #int(Q[0, 0])
-            drone.v_left_right = 0 #= 0#int(Q[0, 1])
-            drone.v_up_dow = 0#"int(Q[0, 2]*1.5)
+            if 75 < drone.yaw < 105:
+                drone.v_forward_backw = int(Q[0, 0])
+                drone.v_left_right = int(Q[0, 1])
+                drone.v_up_dow = int(Q[0, 2]*1.5)
+            else:
+                drone.v_forward_backw = 0
+                drone.v_left_right = 0
+                drone.v_up_dow = 0
+                
+            if drone.cam_timeout:
+                drone.v_forward_backw = 0
+                drone.v_left_right = 0
+                drone.v_yaw = 60
+            else:
+                drone.v_yaw = -int(((drone.yaw - 90)))
+            
+            #print("Fw - bw : ", drone.v_forward_backw, " lft - rgt : ", drone.v_left_right, " u-d : ", drone.v_up_dow)
+            print(drone.yaw)
             drone.testrc()
-            #print("Commande : ", Q)
-            #print("X = ", drone.x, " , Y = ", drone.y, " , Z = ", drone.z)
             time.sleep(0.1)
-            if THREAD_ON == False:
-                break
         time.sleep(0.5)
-        #self.drone.lnd()
+        self.drone.lnd()
+        raise Exception("Atterrissage avec succes !")
 
 
 class CameraThread(threading.Thread):
@@ -88,7 +97,8 @@ class CameraThread(threading.Thread):
         threading.Thread.__init__(self)
         
     def run(self):
-        while THREAD_ON:
+        received_in_row = 0
+        while True:
             data = cam_data_reading.read()
             mutex.acquire()
             try:
@@ -96,15 +106,29 @@ class CameraThread(threading.Thread):
             finally:
                 mutex.release()
             try:
-                drone.x = data["x"]
-                drone.y = data["y"]
-                drone.z = data["z"]
-                drone.yaw = data["yaw"]
+                if data["id"] == 1 or data["id"] == 100:
+                    drone.x = data["x"]
+                    drone.y = data["y"]
+                    drone.z = data["z"]
+                    drone.yaw = data["yaw"]
+                    if data["id"] == 1:
+                        drone.reversed = False
+                    else:
+                        drone.reversed = True
+                elif data["id"] == 101:
+                    drone.platform_coords = {"x": data["x"], "y": data["y"], "z": data["z"], "yaw": data["yaw"]}
             except:
                 drone.x += drone.last_v_forward_backw * (time.time()-drone.last_time_command)
                 drone.y += drone.last_v_left_right * (time.time()-drone.last_time_command)
                 drone.z += drone.last_v_up_dow * (time.time()-drone.last_time_command)
                 drone.z = drone.tello.get_distance_tof()
+            if data:
+                received_in_row = 0
+                drone.cam_timeout = False
+            else:
+                received_in_row += 1
+            if received_in_row > 10:
+                drone.cam_timeout = True
             time.sleep(0.1)
 
 
